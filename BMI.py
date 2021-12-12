840c838f1a049a4cbe205ad0617df1b76233f4cd
unknown <nuttamon21574@gmail.com>
2021-12-12 12:38:20 +0700

    aa

--- BMI.py
+++ BMI.py
@@ -1,12 +1,14 @@
-weight = float(input("Enter your weight : " ))
-height = float(input("Enter your height : " ))
-BMI = weight/(height**2)
+weight = float(input("Enter your weight (kg.) : " ))
+height = float(input("Enter your height (cm.) : " ))
+h_meter = height/100
+BMI = weight/(h_meter**2)
 
+print("ค่า BMI ของคุณคือ: ", BMI)
 
-if BMI < 18.5 :
-  print("underweight")
+if BMI > 24.9 :
+  print("overweight")
 elif 18.5 < BMI < 24.9:
   print("normal")
 else :
-  print("overweight")
+  print("underweight")
 
